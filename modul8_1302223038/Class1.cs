dcd5df0068d4565c16051ec96876c5275631a90b
ArgaAAL <argalumunon9@gmail.com>
2024-04-17 06:47:33 +0700

    commit awal

---
+++ modul8_1302223038/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace modul8_1302223038
+{
+    internal class Class1
+    {
+    }
+}
